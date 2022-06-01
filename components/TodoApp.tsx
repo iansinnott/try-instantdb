cc21c9f2014954ca50d5ee58dabbfee651de1cec
Ian Sinnott <ian@iansinnott.com>
2022-06-01 16:04:42 -0400

    Add post todo list

--- components/TodoApp.tsx
+++ components/TodoApp.tsx
@@ -3,6 +3,13 @@ import { faker } from "@faker-js/faker";
 
 import { transact, useInit, useQuery, tx, id } from "instant-local-throwaway";
 import { title } from "process";
+import Link from "next/link";
+
+enum Status {
+  pending = "pending",
+  complete = "complete",
+  cancelled = "cancelled",
+}
 
 interface ITodo {
   id: string;
@@ -10,17 +17,25 @@ interface ITodo {
   createdAt: Date;
   updatedAt: Date;
   body?: string;
+  status: Status;
 }
 
 type IPayload = Partial<ITodo>;
 
 function upsertTodo(payload: IPayload) {
+  const now = new Date();
+
+  const defaults = {
+    status: Status.pending,
+    title: "Untitled",
+    createdAt: now,
+    updatedAt: now,
+  };
+
   const data: ITodo = {
+    ...defaults,
     ...payload,
     id: payload.id || id(),
-    title: payload.title || "Untitled",
-    createdAt: payload.createdAt || new Date(),
-    updatedAt: payload.updatedAt || payload.createdAt || new Date(),
   };
 
   const comment = { id: id(), body: faker.company.bs() };
@@ -39,15 +54,66 @@ function removeTodos(xs: ITodo[]) {
   transact(models.map((x) => x.delete()));
 }
 
+interface IDataDumper extends React.ComponentProps<"pre"> {
+  data: any;
+}
+export const DataDumper = ({ className, data, ...props }: IDataDumper) => {
+  return (
+    <pre
+      {...props}
+      data-test-id="DataDumper"
+      className={classNames(
+        "font-mono text-sm p-2 mt-8 bg-gray-100 border border-gray-200 rounded",
+        className,
+      )}>
+      {JSON.stringify(data, null, 2)}
+    </pre>
+  );
+};
+
+interface ITodoList extends React.ComponentProps<"div"> {}
+export const TodoList = ({ className, ...props }: ITodoList) => {
+  const q = useQuery({
+    posts: {},
+  });
+  const posts = q.posts as ITodo[];
+  return (
+    <div {...props} data-test-id="TodoList" className={classNames("", className)}>
+      {posts.map((x) => {
+        return (
+          <div className="flex items-center first:border-t border-b  border-zinc-200" key={x.id}>
+            <div className="block flex-shrink-0 flex-grow-0 p-2 bg-gray-200">
+              <input
+                className=""
+                type="checkbox"
+                checked={x.status === Status.complete}
+                onChange={(e) => {
+                  transact([
+                    tx.posts[x.id].update({
+                      status: e.target.checked ? Status.complete : Status.pending,
+                    }),
+                  ]);
+                }}
+              />
+            </div>
+            <Link href={`/todo/${x.id}`}>
+              <a
+                className={classNames("block px-2", {
+                  "line-through opacity-60": x.status === Status.complete,
+                })}>
+                {x.title}
+              </a>
+            </Link>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
 function Main() {
   const q = useQuery({
-    posts: {
-      $: {
-        orderBy: { createdAt: "asc" },
-        limit: 2,
-        offset: 0,
-      },
-    },
+    posts: {},
   });
   const posts = q.posts as ITodo[];
 
@@ -59,7 +125,7 @@ function Main() {
           onClick={() => {
             upsertTodo({ title: faker.company.bs() });
           }}>
-          add a post
+          Add random todo
         </button>
         <button
           className="btn bg-gray-200 hover:bg-red-400"
@@ -69,9 +135,10 @@ function Main() {
           clear
         </button>
       </div>
-      <pre className="font-mono text-sm p-2 mt-8 bg-gray-100 border border-gray-200 rounded">
-        {JSON.stringify(posts, null, 2)}
-      </pre>
+      <div className="my-2">
+        <strong>{posts.length}</strong> posts
+      </div>
+      <TodoList />
     </div>
   );
 }
@@ -80,7 +147,7 @@ type ITodoApp = React.ComponentProps<"div">;
 const TodoApp = ({ className, ...props }: ITodoApp) => {
   const isLoading = useInit({ token: "28848c49-c2c3-499d-b9af-7c1fd396613d" });
   return (
-    <div {...props} data-cy="TodoApp" className={classNames("", className)}>
+    <div {...props} data-test-id="TodoApp" className={classNames("", className)}>
       {isLoading ? <div>Loading ...</div> : <Main />}
     </div>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,8 +8,6 @@ const Home: NextPage = () => {
   return (
     <div className="page-container">
       <h1 className="text-6xl font-bold">Instant Todo List</h1>
-
-      <p className="mt-3 text-2xl">Write something</p>
       <TodoApp />
     </div>
   );
